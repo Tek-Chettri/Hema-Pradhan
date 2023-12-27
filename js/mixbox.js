8b06d790b7c2d8c5c6fb671a34ccd2f5b1548c42
heemapradhan1 <153887774+heemapradhan1@users.noreply.github.com>
2023-12-27 16:30:12 +0530

    D:L:7:added the js for search pop up

--- js/mixbox.js
+++ js/mixbox.js
@@ -7,3 +7,12 @@ $(document).ready(function() {
 	 });
 
 });
+
+$(document).ready(function() {
+
+	$(".fa-search").click(function() {
+	   $(".togglesearch").toggle();
+	   $("input[type='text']").focus();
+	 });
+
+});
